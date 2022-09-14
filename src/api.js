cbfef9a7582f708b94d33655386a1a06ccadd2e5
Rubén Morchón Poveda <ruben.morchon.poveda@outlook.com>
2022-09-14 09:56:17 +0200

    Using dotenv to load twitch keys.
    Changed the API to be able to load .env file variables and put there Twitch's api keys.

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { valorant } from './games/valorant/valorant-rest.js';
 import { twitch } from './twitch/twitch-rest.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
 const rest = express();
 
 export const api = {
